9019d9bdb50ca017a466c1fe5fe2605ccf1151e7
LuisRequejoM <geolurrem@gmail.com>
2024-07-09 18:48:13 +0200

    Prueba

---
+++ src/python/main/Menu.py
@@ -0,0 +1,6 @@
+print("GESTOR PERDIDAS Y GANANCIAS")
+print("--------------------------------")
+print("ELije una de las siguientes opciones: ")
+print("1. Calcular ganancia")
+print("2. Calcular perdida")
+print("3. Volver al menu principal")
