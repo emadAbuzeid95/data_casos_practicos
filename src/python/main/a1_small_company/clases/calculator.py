b72afc368cf7b88999e65d32aa1bc979051e9f76
Amaranto <blasquez1999@gmail.com>
2024-07-10 16:20:13 +0200

    Mensaje de calculadora version 0.1

--- src/python/main/a1_small_company/clases/calculator.py
+++ src/python/main/a1_small_company/clases/calculator.py
@@ -1,6 +1,17 @@
-operacion: str = input('operacion')
+operacion: str = input('operacion\n')
 signos = ("+" in operacion, '-' in operacion, '/' in operacion, '*' in operacion)
+numeros=operacion.split('+')
+if signos[0]==True:
+    numeros = operacion.split('+')
+    print(int(numeros[0])+int(numeros[1]))
+elif signos[1]==True:
+    numeros = operacion.split('-')
+    print(int(numeros[0])-int(numeros[1]))
+elif signos[2]==True:
+    numeros = operacion.split('/')
+    print(int(numeros[0])/int(numeros[1]))
+elif signos[3]==True:
+    numeros = operacion.split('*')
+    print(int(numeros[0])*int(numeros[1]))
 
-match signos:
-    pass
 
