286bf606b53d6009dae2f3f46d0ff5bc7ef6038f
LuisRequejoM <geolurrem@gmail.com>
2024-07-10 17:46:41 +0200

    Borrado de ficheros obsoletos

--- src/python/main/Menu.py
+++
@@ -1,6 +0,0 @@
-print("GESTOR PERDIDAS Y GANANCIAS")
-print("--------------------------------")
-print("ELije una de las siguientes opciones: ")
-print("1. Calcular ganancia")
-print("2. Calcular perdida")
-print("3. Volver al menu principal")
