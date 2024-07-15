a3ad75832a8c53996d702178d549ccbdaef9a8cb
mLanche <marilin475@gmail.com>
2024-07-15 19:03:12 +0200

    Añadir función facturas

--- src/python/main/a1_small_company/clases/facturas.py
+++ src/python/main/a1_small_company/clases/facturas.py
@@ -1,27 +1,90 @@
-#Entrada mostrar
+productos: dict = {123: {"precio": 0.50, "Nombre": "Tuercas"},
+                   132: {"precio": 5.50, "Nombre": "Martillo"},
+                   321: {"precio": 1.50, "Nombre": "Bombilla"},
+                   213: {"precio": 6, "Nombre": "Pala"}
+                   }
 
-saludo:str = """\t\t**Tuerquito Paquito Agradece tu visita**\n\n
-A continuación te mostraré la lista de los productos, con su valor y su código"""
+opcion: int = 0
+total_comprados: dict = {}
+
+while opcion != 5:
+    print(""" **      Welcome to Tuerquito Paquito      ** 
+    Menú:
+    1) Mostrar artículos
+    2) Comprar
+    4) Ver factura final 
+    5) Salir """)
+
+    opcion: int = int(input("Escoja una opción: "))
+    print()
+    match opcion:
+        case 1:
+            for clave, valor in productos.items():
+                print(f"""Artículo: {valor["Nombre"]} 
+                Precio: {valor["precio"]}€
+                Código: {clave}
+                        """)
+
+        case 2:
+            print("""
+            *************** Inicie su pedido ***************
+            """)
+            nombre_cliente: str = str(input("Nombre del cliente: "))
+            codigo_producto: int = int(input("Código del producto: "))
+            cantidad_producto: int = int(input("Cantidad: "))
+
+            if codigo_producto in productos.keys():
+                dicc_compra: dict = {"Código del producto": codigo_producto,
+                                     "Cantidad": cantidad_producto
+                                     }
+                if nombre_cliente in total_comprados.keys():
+                    lista_compra_cliente:list = total_comprados[nombre_cliente]
+                    lista_compra_cliente.append(dicc_compra)
+                    total_comprados[nombre_cliente] = lista_compra_cliente
+
+                else:
+                    total_comprados[nombre_cliente] = [dicc_compra]
+
+                print("""
+                                Pedido realizado
+                                
+                                """)
 
 
-productos =  {"Tuercas": {"precio":0.50, "código": 123},
-     "Martillo": {"precio":5.50, "código": 132},
-     "Bombilla": {"precio":5.50, "código": 321}
-     }
+            else:
+                print("""
+                Código no válido, pruebe otra vez
+                """)
 
 
-for producto in productos:
-    print(producto)
-    for i in productos[producto]:
-        print(f"{i} : {productos[producto][i]}")
+        case 4:
 
-    print("\n")
+            print("""
+            ************* Factura Tuerquito Paquito *************
+            """)
 
+            for nombre_cliente, lista_compra in total_comprados.items():
+                lista_de_total_articulos:list = []
+                for dic in lista_compra:
+                    cantidad: int = dic["Cantidad"]
+                    codigo: int = dic["Código del producto"]
+                    if codigo in productos.keys():
+                        precio:float = productos[codigo]["precio"]
 
+                        total_articulos:float = cantidad * precio
 
+                        lista_de_total_articulos.append(total_articulos)
 
+                total_final:float = sum(lista_de_total_articulos)
 
+                print(f"""
+                        Factura a nombre de Sr/a {nombre_cliente}. {total_final} 
+                    
+                        """)
 
+        case 5:
+            print("""   Ha escogido la opción salir     
+                            Hasta la próxima""")
 
-
-
+        case _:
+            print("Opción no válida")
